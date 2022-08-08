753ed46644293cb29d5a48b6d196d8064db4c044
VorobevOleg <kukuruza-09@ya.ru>
2022-08-08 14:16:46 +0300

    Completed

---
+++ 6kyuCommon/TakeATenMinutesWalk/src/main/java/com/codewars/TestKyu.java
@@ -0,0 +1,14 @@
+package com.codewars;
+
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestKyu {
+    @Test
+    public void Test() {
+        assertEquals("Should return true", true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+    }
+}
